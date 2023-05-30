3f035d5f891c38b8a738547f7d00bd5512f875f1
Manipreeth <manipreethbolusani@gmail.com>
2023-05-30 19:04:09 +0530

    modified import statements

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React,{useState} from 'react'
-import {Button} from 'react-bootstrap'
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./styles.css";
 
-import Rules from './rules'
-import Game from './crackTheCode'
+import Rules from "./Components/Rules";
+import Game from "./Components/crackTheCode";
 
 export default function App() {
-const [gameInterface,handleGameInterface] =useState(true)
+  const [gameInterface, handleGameInterface] = useState(true);
 
-const clicked = ()=>{
-  handleGameInterface(false)
-}
+  const clicked = () => {
+    handleGameInterface(false);
+  };
   return (
-    <div className='App'>
-    
-    {gameInterface ?
-        <div id='rulesDisplay'>
-          <Rules/>
-          <Button variant="success" onClick={clicked}>I'm ready to break the code</Button>
+    <div className="App">
+      {gameInterface ? (
+        <div id="rulesDisplay">
+          <Rules />
+          <Button variant="success" onClick={clicked}>
+            I'm ready to break the code
+          </Button>
         </div>
-     :
-        <Game/>
-    }
+      ) : (
+        <Game />
+      )}
     </div>
   );
 }
